a8613ed0ee021326963f364db3088a35a68f2600
MrHuangJser <565628147@qq.com>
2020-06-14 21:38:58 +0800

    MrHuangJser - fix ts config issue

--- src/renderer/app/app.module.ts
+++ src/renderer/app/app.module.ts
@@ -1,38 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { IconDefinition } from '@ant-design/icons-angular';
 import * as AllIcons from '@ant-design/icons-angular/icons';
-import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { NzCardModule, NzLayoutModule, NzMenuModule, NzToolTipModule } from 'ng-zorro-antd';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { AppComponent } from './app.component';
 import { routes } from './app.routing';
 import { ElectronService } from './core/services/electron.service';
 import { TitleComponent } from './layouts/title/title.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NodeInfoComponent } from './pages/node-list/node-info/node-info.component';
 import { NodeListComponent } from './pages/node-list/node-list.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const antDesignIcons = AllIcons as {
   [key: string]: IconDefinition;
 };
 const icons: IconDefinition[] = Object.keys(antDesignIcons).map((key) => antDesignIcons[key]);
 
+const ANTD_MODULES = [NzIconModule.forRoot(icons), NzToolTipModule, NzMenuModule, NzLayoutModule, NzCardModule];
+
 const LAYOUTS = [TitleComponent];
 
-const PAGES = [HomeComponent, NodeListComponent];
+const PAGES = [HomeComponent, NodeListComponent, NodeInfoComponent];
 
 @NgModule({
   declarations: [AppComponent, ...PAGES, ...LAYOUTS],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    CommonModule,
-    RouterModule.forRoot(routes),
-    NzIconModule.forRoot(icons),
-    NgZorroAntdModule,
-  ],
+  imports: [BrowserModule, BrowserAnimationsModule, CommonModule, RouterModule.forRoot(routes), ...ANTD_MODULES],
   providers: [],
   bootstrap: [AppComponent],
 })
