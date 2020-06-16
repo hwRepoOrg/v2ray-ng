38f7ef7bb344934e8c8bbf1d04941164dc7be4ec
MrHuangJser <565628147@qq.com>
2020-06-16 22:12:39 +0800

    MrHuangJser - add ng zorro module

--- src/renderer/app/app.module.ts
+++ src/renderer/app/app.module.ts
@@ -5,11 +5,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { IconDefinition } from '@ant-design/icons-angular';
 import * as AllIcons from '@ant-design/icons-angular/icons';
-import { NzCardModule, NzLayoutModule, NzMenuModule, NzToolTipModule } from 'ng-zorro-antd';
+import {
+  NzCardModule,
+  NzFormModule,
+  NzLayoutModule,
+  NzMenuModule,
+  NzRadioModule,
+  NzToolTipModule,
+  NzTagModule,
+} from 'ng-zorro-antd';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { AppComponent } from './app.component';
 import { routes } from './app.routing';
-import { ElectronService } from './core/services/electron.service';
+import { ElectronService } from './services/electron.service';
 import { TitleComponent } from './layouts/title/title.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NodeInfoComponent } from './pages/node-list/node-info/node-info.component';
@@ -20,7 +28,16 @@ const antDesignIcons = AllIcons as {
 };
 const icons: IconDefinition[] = Object.keys(antDesignIcons).map((key) => antDesignIcons[key]);
 
-const ANTD_MODULES = [NzIconModule.forRoot(icons), NzToolTipModule, NzMenuModule, NzLayoutModule, NzCardModule];
+const ANTD_MODULES = [
+  NzIconModule.forRoot(icons),
+  NzToolTipModule,
+  NzMenuModule,
+  NzLayoutModule,
+  NzCardModule,
+  NzFormModule,
+  NzRadioModule,
+  NzTagModule,
+];
 
 const LAYOUTS = [TitleComponent];
 
