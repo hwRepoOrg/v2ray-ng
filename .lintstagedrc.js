98326472ebf4b295fd2d744963f7f8584ad5bbb3
MrHuangJser <565628147@qq.com>
2020-07-10 14:52:21 +0800

    MrHuangJser - update linter config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -11,7 +11,7 @@ module.exports = {
     `./node_modules/.bin/tslint --fix -p ./src/renderer/tsconfig.json ${files
       .map((file) => file.replace(process.cwd() + '/', ''))
       .join(' ')}`,
-    `./node_modules/.bin/ng lint --fix  ${files
+    `./node_modules/.bin/ng lint v2ray-ng --fix  ${files
       .map((file) => '--files ' + file.replace(process.cwd() + '/', ''))
       .join(' ')}`,
     `./node_modules/.bin/prettier --write ${files.map((file) => file.replace(process.cwd() + '/', '')).join(' ')}`,
