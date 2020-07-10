1adacf5cbee985cc6d7118ffb03c4a67c5948541
MrHuangJser <565628147@qq.com>
2020-07-10 12:31:42 +0800

    MrHuangJser - add git hook

---
+++ .lintstagedrc.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+module.exports = {
+  'src/main/**/*.ts': (files) => [
+    `./node_modules/.bin/prettier --write ${files.map((file) => file.replace(process.cwd() + '/', '')).join(' ')}`,
+  ],
+  'src/renderer/**/*.ts': (files) => [
+    `./node_modules/.bin/tslint --fix -p ./src/renderer/tsconfig.json ${files
+      .map((file) => file.replace(process.cwd() + '/', ''))
+      .join(' ')}`,
+    `./node_modules/.bin/ng lint --fix  ${files
+      .map((file) => '--files ' + file.replace(process.cwd() + '/', ''))
+      .join(' ')}`,
+    `./node_modules/.bin/prettier --write ${files.map((file) => file.replace(process.cwd() + '/', '')).join(' ')}`,
+  ],
+  'src/**/*.html': (files) => [
+    `./node_modules/.bin/prettier --write ${files.map((file) => file.replace(process.cwd() + '/', '')).join(' ')}`,
+  ],
+  'src/**/*.less': ['npm run lint:style', './node_modules/.bin/prettier --write'],
+};
