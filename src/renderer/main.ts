65523f8fe7de2a2a6c2cb98dc9cc610246ea3e18
MrHuangJser <565628147@qq.com>
2020-07-14 10:22:17 +0800

    MrHuangJser - remove log redirect

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,18 +1,8 @@
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { environment } from '../environments/environment';
 import { AppModule } from './app/app.module';
 
 // tslint:disable-next-line: no-string-literal
 window['ELECTRON_DISABLE_SECURITY_WARNINGS'] = true;
-
-console.log = window.require('electron').remote.getGlobal('console').log;
-console.warn = window.require('electron').remote.getGlobal('console').warn;
-console.error = window.require('electron').remote.getGlobal('console').error;
-
-if (environment.production) {
-  enableProdMode();
-}
 
 try {
   platformBrowserDynamic()
