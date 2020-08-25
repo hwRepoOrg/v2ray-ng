06d7b5b0552af48f82ce701617078e83a0c6d1c9
MrHuangJser <565628147@qq.com>
2020-08-25 18:05:06 +0800

    MrHuangJser - remove unused file

--- src/renderer/app/interfaces/table-column.interface.ts
+++
@@ -1,6 +0,0 @@
-import { STColumn } from '@delon/abc/st';
-
-// @ts-ignore
-export interface ISTColumn<T = any> extends STColumn {
-  index: keyof T | string | string[];
-}
--- src/renderer/app/pages/subscribe-list/subscribe-form/subscribe-form.component.ts
+++ src/renderer/app/pages/subscribe-list/subscribe-form/subscribe-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ISFSchema } from '@renderer/interfaces/form-schema.interface';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ISubscribeConfig } from '@typing/config.interface';
 
 @Component({
@@ -8,27 +8,20 @@ import { ISubscribeConfig } from '@typing/config.interface';
   styleUrls: ['./subscribe-form.component.less'],
 })
 export class SubscribeFormComponent implements OnInit {
-  public schema: ISFSchema<ISubscribeConfig> = {
-    type: 'object',
-    required: ['title', 'url'],
-    properties: {
-      title: { type: 'string', title: '名称' },
-      url: {
-        type: 'string',
-        title: '订阅地址',
-        ui: {
-          widget: 'textarea',
-          autosize: {
-            minRows: 3,
-          },
-        },
-      },
-    },
-  };
+  public formGroup: FormGroup;
   @Input() record: Partial<ISubscribeConfig>;
   @Output() formSubmit = new EventEmitter<Partial<ISubscribeConfig>>();
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {
+    this.formGroup = this.fb.group({
+      title: [, [Validators.required]],
+      url: [, [Validators.required]],
+    });
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.record) {
+      this.formGroup.patchValue(this.record);
+    }
+  }
 }
